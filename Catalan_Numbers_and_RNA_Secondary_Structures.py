0d13b00a7a7d134d40862c95eee5dce1c81db878
Luyi Tian <tlytiger@hotmail.com>
2014-11-04 05:20:22 +0800

    change my algo
    inspired by jschendel, with the same thoughts but my method has bugs

--- Catalan_Numbers_and_RNA_Secondary_Structures.py
+++ Catalan_Numbers_and_RNA_Secondary_Structures.py
@@ -1,52 +1,43 @@
 ####Catalan Numbers and RNA Secondary Structures
+
+#NOTE: main algo is from : https://github.com/jschendel/Rosalind/blob/master/033_CAT.py
+#TODO: fix bugs of my original code
 comp = {'A':'U', 'U':'A', 'C':'G', 'G':'C'}
 def eq_GC(st):
-    if (st.count('A')+st.count('U'))%2 == 0:return True
+    if (st.count('A')==st.count('U')) and (st.count('G')==st.count('C')):return True
     else: return False
-file_path = '/Users/luyi/Downloads/rosalind_cat (1).txt'
+file_path = '/Users/luyi/Downloads/rosalind_cat (2).txt'
 RNA = ''
 
 for line in open(file_path):
     if line[0] == '>':continue
     RNA+=line.strip()
+def check_subinterval(subint):
+    '''Checks if a given subinterval has the same number of matching nucleotides.'''
+    N = [subint.count(nucleotide) for nucleotide in 'AUCG']
+    return N[0] == N[1] and N[2] == N[3]  # Necessary for a noncrossing perfect bonding.
 
 
-solved_dict = {}
-def find_match(S,lo,up):
-    if up-lo ==1:
-        if S[up] == comp[S[lo]]:
+def noncrossing_perfect_bondings(rna):
+    '''Returns the number of noncrossing perfect bondings for the given RNA sequence'''
+    # Initialize dictionaries.
+    bonding = {'A':'U', 'U':'A', 'C':'G', 'G':'C'}
+    noncrossing_bondings = {}
+
+    def count_noncrossing(rna):
+        '''Recursively computes the number of noncrossing perfect bondings for a given RNA sequence'''
+        if len(rna) <= 2:  # We only send valid rna matchings, so this return is ok.
             return 1
-        else: return 0
-    for bp in range(lo+1,up+1,2):
-        #print 'bp:',bp
-        if S[lo] == comp[S[bp]]:
-            #print '  match'
-            #print S[bp+1:up+1]
-            if eq_GC(S[lo:bp+1]):
-                if bp == up:
-                    if solved_dict.has_key(S[lo+1:bp-1]):
-                        solved_dict[S[lo:up+1]]+=solved_dict[S[lo+1:bp-1]]
-                    else:
-                        solved_dict[S[lo+1:bp-1]] = find_match(S,lo+1,up-1)
-                        print solved_dict[S[lo+1:bp-1]]
-                        solved_dict[S[lo:up+1]]+=solved_dict[S[lo+1:bp-1]]
-                    #print '  RES',RES
-                #print '    eq_gc',RES
-                if solved_dict.has_key(S[lo:bp+1]):
-                    if solved_dict.has_key(S[bp+1:up+1]):
-                        pass
-                    else:
-                        solved_dict[S[bp+1:up+1]] = find_match(S,bp+1,up)
-                else:
-                    if solved_dict.has_key(S[bp+1:up+1]):
-                        solved_dict[S[lo:bp+1]] = find_match(S,lo,bp)
-                    else:
-                        solved_dict[S[lo:bp+1]] = find_match(S,lo,bp)
-                        solved_dict[S[bp+1:up+1]] = find_match(S,bp+1,up)
-                solved_dict[S[lo:up+1]]+=solved_dict[S[lo:bp+1]]*solved_dict[S[bp+1:up+1]]
-                #print '  RES',RES
-    if solved_dict.has_key(S[lo:up+1]):
-        return solved_dict[S[lo:up+1]]
-    else:
-        return 0
-print find_match(RNA,0,len(RNA)-1)%1000000
+        elif rna in noncrossing_bondings:  # If we've already computed the value, return it!
+            return noncrossing_bondings[rna]
+
+        # Determine valid locations where the RNA can be split while maintaining necessary noncrossing conditions.
+        splits = [i for i in xrange(1, len(rna), 2) if rna[0] == bonding[rna[i]] and check_subinterval(rna[1:i])]
+
+        # Reduce the problem to noncrossing matchings over the subintervals.
+        noncrossing_bondings[rna] = sum([count_noncrossing(rna[1:i])*count_noncrossing(rna[i+1:]) for i in splits]) 
+
+        return noncrossing_bondings[rna]
+
+    return count_noncrossing(rna)
+print noncrossing_perfect_bondings(RNA)%1000000
